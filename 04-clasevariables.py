64702e0b6fef5bc7165ef397db8cc8b522b69d7e
Angela Santacruz <sabrinyty@hotmail.com>
2024-07-26 22:46:57 -0500

    ejercicio variables 1

--- 04-clasevariables.py
+++ 04-clasevariables.py
@@ -13,7 +13,7 @@ edad2 = 50
 #print(edad1 + edad2)
 
 nombre = input("Dime tu nombre: ")
-#print("Tu nombre es: " + nombre)
+print("Tu nombre es: " + nombre)
 
 nombre1 = "John"
 nombre2 = "Jose"
---
+++ ejercicio1variables.py
@@ -0,0 +1,6 @@
+pregunta1= input("A que forma cosmica te recuerda tomar una cerveza: ")
+print("La forma cosmica que te recuerda tomar una cerveza es: " + pregunta1)
+pregunta2= input("A que color te sabe una cerveza: ")
+print("La cerveza te sabe al color: " + pregunta2)
+preguntas = pregunta1 + " " + pregunta2
+print("el nombre de la cerveza es " + preguntas)
